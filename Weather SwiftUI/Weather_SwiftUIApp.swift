f2f5c9390bb00e0f010a8211e01f8d1c84dce2cd
IsmoilXolmatov <ismoilxolmatov69@gmail.com>
2023-08-27 21:32:02 +0500

    Initial Commit

---
+++ Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by Khalmatov on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
